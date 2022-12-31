de8a2a1cbd7fbaedbc70968bb956c00ab9ac2d9a
rahul100719 <rr10071997@gmail.com>
2022-12-31 16:47:25 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import { Toaster } from 'react-hot-toast'
+import Layout from '../componets/Layout'
+import { StateContext } from '../context/stateContext'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+  <Layout> 
+    <Toaster/> 
+    <Component {...pageProps} />
+  </Layout>
+  </StateContext>
+)
 }
